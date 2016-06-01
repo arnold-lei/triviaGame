445dad51c32c58a4c73330da5e4e3630bb985b50
Arnold Lei <arnold.lei88@gmail.com>
2016-06-01 01:17:39 -0400

    added win loss alert logic

--- assets/js/game.js
+++ assets/js/game.js
@@ -6,7 +6,7 @@ $('document').ready(function(){
   //Where the question will be printed and answer options
   var questDiv = $('.question');
   var options = $('.options');
-
+  var alert = $('.alert');
   // Question array
   var questions = [
     {
@@ -41,9 +41,20 @@ $('document').ready(function(){
 
   //Loop question options
   for(var i = 0; i < questions[questionCounter].options.length ; i++){
-    var newDiv = $('<button class="btn btn-primary" value="'+i+'">'+ questions[questionCounter].options[i] + '</button>');
+    var newDiv = $('<button class="btn btn-primary guess" value="'+i+'">'+ questions[questionCounter].options[i] + '</button>');
     options.append(newDiv);
-
   }
+  $('.guess').on('click', function(){
+    console.log(questions[questionCounter].answer);
+    if(this.value == questions[questionCounter].answer){
+      alert.removeClass('hidden');
+      alert.addClass('alert-success');
+      alert.html('You have gotten the correct answer!');
+    }else{
+      alert.removeClass('hidden');
+      alert.addClass('alert-danger');
+      alert.html('Sorry have gotten the wrong answer! The correct answer was: ' + questions[questionCounter].options[questions[questionCounter].answer] );
+    }
+  });
 
 });
